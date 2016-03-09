51f260f1696a8698be53c5484fc4185d2e2e2a5c
George Bott <insertlogic@gmail.com>
2016-03-09 02:45:24 +0000

    Added update support

--- Client/Client/App.xaml.cs
+++ Client/Client/App.xaml.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +17,54 @@ namespace Client
     /// </summary>
     public partial class App : Application
     {
+        public enum UpdateState
+        {
+            Nothing,
+            UpdateCompleted,
+            UpdateFailed
+        }
+
+        public UpdateState ProgramUpdateState = UpdateState.Nothing;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            switch (e.Args.Length)
+            {
+                case 1:
+                    switch (e.Args[1])
+                    {
+                        case "--updateCompleted":
+                            ProgramUpdateState = UpdateState.UpdateCompleted;
+                            break;
+                        case "--updateFailed":
+                            ProgramUpdateState = UpdateState.UpdateFailed;
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (e.Args[1])
+                    {
+                        case "--updateMove":
+                            int updateAttempts = 3;
+                            while (updateAttempts-- > 0)
+                            {
+                                try
+                                {
+                                    File.Copy(Assembly.GetExecutingAssembly().Location, e.Args[2]);
+                                    Process.Start(e.Args[2], "--updateCompleted");
+                                    Shutdown();
+                                }
+                                catch
+                                {
+                                    Thread.Sleep(5000);
+                                }
+                            }
+                            Process.Start(e.Args[2], "--updateFailed");
+                            Shutdown();
+                            break;
+                    }
+                    break;
+            }
+        }
     }
 }
--- Client/Client/MainWindow.xaml.cs
+++ Client/Client/MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -25,9 +27,30 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool _stopUpdate = false;
+        Thread _updateCheckThread;
+        string _updateBinaryUrl;
+        Version _updateVersion;
+        Thread _updateDownloadThread;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            switch((Application.Current as App).ProgramUpdateState)
+            {
+                case App.UpdateState.UpdateCompleted:
+                    updateCompleteStatus.Foreground = Brushes.Green;
+                    updateCompleteStatus.Content = $"Successfully updated to version {Assembly.GetExecutingAssembly().GetName().Version}";
+                    break;
+                case App.UpdateState.UpdateFailed:
+                    updateCompleteStatus.Foreground = Brushes.Red;
+                    updateCompleteStatus.Content = "Update failed, could not copy new update binary, make sure you are not running extra instances of the program";
+                    break;
+                case App.UpdateState.Nothing:
+                    updateCompleteStatus.Visibility = Visibility.Collapsed;
+                    break;
+            }
 
             osuInstallationInput.Text = string.Empty;
 
@@ -39,10 +62,67 @@ namespace Client
 
             trawlingPageLabel.Visibility = Visibility.Collapsed;
             trawlingErrorLabel.Visibility = Visibility.Collapsed;
+
+            updateButton.Visibility = Visibility.Collapsed;
+            updateStatus.Visibility = Visibility.Collapsed;
+            updateProgress.Visibility = Visibility.Collapsed;
+            updateException.Visibility = Visibility.Collapsed;
+
+            _updateCheckThread = new Thread(() =>
+            {
+                while (!_stopUpdate)
+                {
+                    try
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        var localVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+                        var request = WebRequest.CreateHttp("https://api.github.com/repos/Zogzer/BloodCatTrawler/releases/latest");
+                        request.UserAgent = $"BloodCatTrawler/{localVersion}";
+                        using (var response = request.GetResponse())
+                        using (var stream = response.GetResponseStream())
+                        using (var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max))
+                            doc.Load(reader);
+
+                        var root = doc.SelectSingleNode("root");
+                        var versionString = root["tag_name"].InnerText;
+
+                        var versionBuffer = new int[4] { 0, 0, 0, 0 };
+                        var versionSegments = Array.ConvertAll(versionString.Split('.'), int.Parse);
+                        Array.Copy(versionSegments, versionBuffer, versionSegments.Length);
+
+                        var latestVersion = new Version(versionBuffer[0], versionBuffer[1], versionBuffer[2], versionBuffer[3]);
+
+                        if (latestVersion > localVersion)
+                        {
+                            _updateBinaryUrl = root["assets"].FirstChild["browser_download_url"].InnerText;
+                            _updateVersion = latestVersion;
+
+                            Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                updateStatus.Visibility = Visibility.Visible;
+                                updateStatus.Content = $"New update (version {latestVersion}) is avaliable";
+
+                                updateButton.Visibility = Visibility.Visible;
+                                updateButton.Content = "Update";
+                            }));
+                        }
+
+                        _stopUpdate = true;
+                    }
+                    catch
+                    {
+                        Thread.Sleep(10000); // Idk if it is worth doing anything more than this
+                    }
+                }
+            });
+            _updateCheckThread.Start();
         }
 
         private void osuInstallationConfirm_Click(object sender, RoutedEventArgs e)
         {
+            updateCompleteStatus.Visibility = Visibility.Collapsed;
+
             osuInstallationWrapper.IsEnabled = false;
             osuInstallationStatus.Visibility = Visibility.Collapsed;
             osuInstallationConfirm.Visibility = Visibility.Collapsed;
@@ -229,6 +309,9 @@ namespace Client
             }
             else
             {
+                updateButton.Content = "Stop song downloading to update";
+                updateButton.IsEnabled = false;
+
                 startStopButton.Content = "Starting";
                 startStopButton.IsEnabled = false;
 
@@ -285,53 +368,49 @@ namespace Client
 
                         try
                         {
+                            XmlDocument doc = new XmlDocument();
+
                             var request = WebRequest.CreateHttp(uri);
                             using (var response = request.GetResponse())
                             using (var stream = response.GetResponseStream())
+                            using (var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max))
+                                doc.Load(reader);
+
+                            var songs = doc.SelectNodes("/root/item");
+
+                            if (songs.Count == 0)
+                                break;
+
+                            int newCount = 0;
+                            foreach (XmlNode s in songs)
                             {
-                                XmlDocument doc = new XmlDocument();
-                                using (var reader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max))
-                                {
-                                    XElement xml = XElement.Load(reader);
-                                    doc.LoadXml(xml.ToString());
-                                }
+                                var info = new SongInfo(long.Parse(s["id"].InnerText), s["title"].InnerText);
 
-                                var songs = doc.SelectNodes("/root/item");
-
-                                if (songs.Count == 0)
-                                    break;
-
-                                int newCount = 0;
-                                foreach (XmlNode s in songs)
-                                {
-                                    var info = new SongInfo(long.Parse(s["id"].InnerText), s["title"].InnerText);
-
-                                    if (_localSongs.Contains(info, new InlineEqualityComparer<SongInfo>((x, y) => x.ID == y.ID)))
+                                if (_localSongs.Contains(info, new InlineEqualityComparer<SongInfo>((x, y) => x.ID == y.ID)))
                                     continue;
 
-                                     if (_processingSongs.Contains(info, new InlineEqualityComparer<SongInfo>((x, y) => x.ID == y.ID)))
+                                if (_processingSongs.Contains(info, new InlineEqualityComparer<SongInfo>((x, y) => x.ID == y.ID)))
                                     continue;
 
-                                     _processingSongs.Add(new ProcessedSongInfo(info, ProcessedSongInfo.ProcessState.Waiting));
-                                    newCount++;
-                                }
-
-                                if (newCount == 0)
-                                    if (noNew)
-                                    {
-                                        page++;
-                                        SetTrawlingPageLabel(page);
-                                        noNew = false;
-                                    }
-                                    else
-                                        noNew = true;
+                                _processingSongs.Add(new ProcessedSongInfo(info, ProcessedSongInfo.ProcessState.Waiting));
+                                newCount++;
                             }
+
+                            if (newCount == 0)
+                                if (noNew)
+                                {
+                                    page++;
+                                    SetTrawlingPageLabel(page);
+                                    noNew = false;
+                                }
+                                else
+                                    noNew = true;
 
                             ProcessedSongInfo song = null;
                             while (!_stopRequested && (song = _processingSongs.FirstOrDefault(x => x.State != ProcessedSongInfo.ProcessState.Completed)) != null)
                             {
                                 song.State = ProcessedSongInfo.ProcessState.Downloading;
-                                Dispatcher.BeginInvoke(new Action(() => { processingSongsListBox.Items.Refresh(); }));
+                                Dispatcher.BeginInvoke(new Action(processingSongsListBox.Items.Refresh));
 
                                 using (var client = new WebClient())
                                 {
@@ -381,11 +460,14 @@ namespace Client
                                 s.State = ProcessedSongInfo.ProcessState.Waiting;
                         }
 
-                        Dispatcher.BeginInvoke(new Action(() => { processingSongsListBox.Items.Refresh(); }));
+                        Dispatcher.BeginInvoke(new Action(processingSongsListBox.Items.Refresh));
                     }
 
                     Dispatcher.BeginInvoke(new Action(() =>
                     {
+                        updateButton.IsEnabled = true;
+                        updateButton.Content = "Update";
+
                         startStopButton.Content = "Start";
                         startStopButton.IsEnabled = true;
                         searchTermsWrapper.IsEnabled = true;
@@ -406,9 +488,76 @@ namespace Client
             Hide();
 
             _stopRequested = true;
+            _stopUpdate = true;
 
             if (_workingThread != null)
                 _workingThread.Join();
+
+            if (_updateCheckThread != null)
+                _updateCheckThread.Join();
+
+            if (_updateDownloadThread != null)
+                _updateDownloadThread.Join();
         }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            osuInstallationWrapper.IsEnabled = false;
+            programBodyWrapper.IsEnabled = false;
+
+            updateButton.Visibility = Visibility.Collapsed;
+            updateProgress.Visibility = Visibility.Visible;
+
+            _updateDownloadThread = new Thread(() =>
+            {
+                using (var client = new WebClient())
+                {
+                    var fileName = Assembly.GetExecutingAssembly().GetName().Name + "." + _updateVersion.ToString() + ".exe";
+                    var tempPath = Path.Combine(Path.GetTempPath(), fileName);
+
+                    client.DownloadProgressChanged += (object dSender, DownloadProgressChangedEventArgs dE) =>
+                    {
+                        Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            if (dE.ProgressPercentage != updateProgress.Value)
+                                updateProgress.Value = dE.ProgressPercentage;
+                        }));
+                    };
+                    int tryCount = 0;
+                    while (true)
+                    {
+                        tryCount++;
+                        Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            updateStatus.Content = "Downloading update" + (tryCount > 1 ? ", attempt " + tryCount : string.Empty);
+                            updateException.Visibility = Visibility.Collapsed;
+                        }));
+
+                        try
+                        {
+                            client.DownloadFileTaskAsync(new Uri(_updateBinaryUrl), tempPath).Wait();
+
+                            Process.Start(tempPath, $"--updateMove {tempPath}");
+
+                            Dispatcher.BeginInvoke(new Action(Close));
+
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                updateException.Visibility = Visibility.Visible;
+                                updateException.Content = ex.Message;
+                            }));
+
+                            Thread.Sleep(5000);
+                        }
+                    }
+                }
+            });
+            _updateDownloadThread.Start();
+        }
+
     }
 }
